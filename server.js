b7bf0a90c26efb70a142a281d02a1f7fe10f5b43
Slava Makeev <unclearnuclear@yahoo.com>
2023-04-06 00:38:29 +0000

    minor auth additions (comm-/out)

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+//const verifyUser = require('./auth');
 const Image = require('./models/schema');
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+//app.use(verifyUser);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 mongoose.connect(process.env.DB_URL);
 
